1050eb8f5749db46a6b402f2d8a8d124a0e46bba
Torben <59419684+entorb@users.noreply.github.com>
2025-02-09 09:01:36 +0100

    html comparison: use latest release instead of WorkInProgress

--- scripts/ebook/step_8.sh
+++ scripts/ebook/step_8.sh
@@ -9,8 +9,9 @@ source_file="hpmor.html"
 target_file="hpmor-html-diff.log"
 
 echo ==== 8.1 downloading from latest release ====
-wget --quiet https://github.com/entorb/hpmor-de/releases/download/WorkInProgress/hpmor.html -O hpmor-prev.html
-# or use latest release, like https://github.com/rrthomas/hpmor/releases/latest/download/hpmor.html
+wget --quiet https://github.com/entorb/hpmor-de/releases/latest/download/hpmor.html -O hpmor-prev.html
+# latest release https://github.com/entorb/hpmor-de/releases/latest/download/hpmor.html
+# WorkInProgress https://github.com/entorb/hpmor-de/releases/download/WorkInProgress/hpmor.html
 
 echo ==== 8.2 diff ====
 diff -U 0 -s hpmor-prev.html $source_file >$target_file
