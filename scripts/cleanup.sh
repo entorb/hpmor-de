5e516203b88a6d7ffcb19c974933b669f430d9fd
Torben <59419684+entorb@users.noreply.github.com>
2026-08-20 08:25:46 +0200

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,9 +2,17 @@
 
 # ensure we are in the hpmor root dir
 script_dir=$(cd $(dirname $0) && pwd)
-cd $script_dir/..
+cd "$script_dir"/..
 
 latexmk -C
-rm -rf chapters/*.aux
+rm -rf *.pdf
+rm -rf *.log
 rm -rf chapters/*-autofix.tex
+rm -rf chapters/*.aux
+rm -rf hpmor-prev.html
+rm -rf hpmor.docx
+rm -rf hpmor.epub
+rm -rf hpmor.fb2
+rm -rf hpmor.html
+rm -rf hpmor.mobi
 rm -rf tmp/
